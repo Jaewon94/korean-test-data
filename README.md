2bffa229b21c5765840451d8d8507c5ed657e44e
Jaewon94 <wodnjs3580@naver.com>
2025-01-30 14:19:45 +0900

    chore(init): 스프링 부트 프로젝트 초기화
    - vscode에서 Spring Initializr를 통해 프로젝트 초기화
    - 당장 필요할 것으로 예상되는 디펜던시 추가
    
    Closes #5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # korean-test-data
+
 Mokaroo 서비스를 벤치마크한 한글 테스트 데이터 생성 웹사이트 만들기
---
+++ src/main/java/com/fastcampus/korean_test_data/KoreanTestDataApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.korean_test_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanTestDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreanTestDataApplication.class, args);
+	}
+
+}
